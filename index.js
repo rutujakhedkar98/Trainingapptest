5ccc7b0af758df517273a601c8c84bfd55f4b9db
RUTUJA PRABHAKARRAO KHEDKAR <116660267+rutujakhedkar98@users.noreply.github.com>
2023-10-25 13:32:28 +0530

    final code

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const app = require('./app');
+const mongoose = require('mongoose');
+const path  = require('path');
+const routes  = require('./routes/index');
+const bodyParser = require('body-parser');
+
+
+
+//
+// database connection
+mongoose.connect(process.env.DB_LOCAL_URL).then(() => {
+    console.log(`Database is connected.`);
+});
+
+// server
+const port = process.env.PORT || 5600;
+
+app.listen(port, () => {
+    console.log(`SERVER IS RUNNING ON PORT ${port}`);
+});
